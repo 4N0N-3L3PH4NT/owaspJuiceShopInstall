f078738a234b83b54dcc5958d708e72c5127a33d
4N0N-3L3PH4NT <4N0N.3L3PH4NT@gmail.com>
2020-12-26 18:56:05 +0100

    added url handling

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 
 echo 'This can take a few minutes to be installed. So grab a coffee and relax!!'
 
+
+
+#check for command line arguments 
+if [ "$1" = "" ]; then
+    echo 'installing owasp juice shop in current directory.'
+elif [ "$1" != "" ]; then
+    echo 'installing owasp juice shop in '$1
+    cd $1
+    
+fi
+
 git clone https://github.com/bkimminich/juice-shop.git
 cd juice-shop
 npm install
 npm start
-
